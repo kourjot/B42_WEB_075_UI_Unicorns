283f4bcf1bb2438665b99b8b94064e140227dbba
kourjot <pkour6188@gmail.com>
2025-02-05 02:05:38 +0530

    🌐 Add: Implement CORS middleware for cross-origin requests 🔒

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ import express from "express"
 import "dotenv/config"
 import{connection} from "./config/db.js"
 import {userRouter} from "./router/userRouter.js"
+import cors from "cors"
 const app=express()
 app.use(express.json())
+app.use(cors())
 app.use(userRouter)
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on ${process.env.PORT}`)
