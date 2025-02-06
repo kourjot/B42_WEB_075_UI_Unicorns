b40bb093f260976ff3b53f3ac24dd1b888fe18a3
Harshit <harshit619131@gmail.com>
2025-02-06 18:10:44 +0530

    Added getBuddies!

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -2,7 +2,8 @@ import {Router} from "express"
 import {signIn,login} from "../controller/userController.js"
 import {validatesigIn,validateLogin} from "../middlewere/validateUser.js"
 import { profileData } from "../controller/profileController.js";
-import { getProfile } from "../controller/profileController.js"; 
+import { getProfile } from "../controller/profileController.js";
+import { getBuddies } from "../controller/matchBuddiesController.js";
 
 const userRouter =Router()
 userRouter.post("/signin",validatesigIn,signIn)
@@ -10,4 +11,5 @@ userRouter.post("/login",validateLogin,login)
 
 userRouter.post("/profile",profileData)
 userRouter.get("/getProfile",getProfile)
+userRouter.get("/getBuddies",getBuddies)
 export {userRouter}
