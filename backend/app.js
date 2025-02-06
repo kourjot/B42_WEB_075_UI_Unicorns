f5c95a0b4eb768125a188cf7d0e95d5003135671
kourjot <pkour6188@gmail.com>
2025-02-06 12:08:07 +0530

    new change

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,15 @@ import "dotenv/config"
 import{connection} from "./config/db.js"
 import {userRouter} from "./router/userRouter.js"
 import cors from "cors"
-// import  {passport}  from "./googleAuth/auth.js"
-// import { googleRouter} from "./googleAuth/oAuthRouter.js";
+import  {passport}  from "./googleAuth/auth.js"
+import { googleRouter} from "./googleAuth/oAuthRouter.js";
 const app=express()
 const PORT= process.env.PORT || 3110;
 app.use(express.json())
 app.use(cors())
 
 app.use(userRouter)
-// app.use("/oauth",googleRouter)
+app.use("/oauth",googleRouter)
 app.use("/login",(req,res)=>{
     res.json({msg:"user login successful with oAuth credentials"})
 })
