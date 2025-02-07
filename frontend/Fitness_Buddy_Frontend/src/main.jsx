f14e346a823d348f6c8967f486fd7f8902000678
kourjot <pkour6188@gmail.com>
2025-02-07 19:57:01 +0530

    deploy

--- frontend/Fitness_Buddy_Frontend/src/main.jsx
+++ frontend/Fitness_Buddy_Frontend/src/main.jsx
@@ -1,11 +1,10 @@
 
 import React from 'react';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext'; // Import your AuthProvider
-import React  from 'react'
+
 
 
 createRoot(document.getElementById("root")).render(
