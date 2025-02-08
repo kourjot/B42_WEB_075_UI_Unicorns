45bdf8792b57469392a952ae4dbbc26a6371ed7b
Harshit <harshit619131@gmail.com>
2025-02-09 03:07:11 +0530

    Gym and Fitness Class updated!

--- backend/model/fitnessClassModel.js
+++ backend/model/fitnessClassModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const fitnessClassSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    location: { type: String, required: true }, // Stores the city name directly
+    city: { type: String, required: true }, // Stores the city name directly
     schedule: { type: String, required: true }, // Example: "Monday, Wednesday at 6 PM"
     instructor: { type: String, required: true }
 });
