d829c45ed954aa48375c2b2dbb244a9995d92005
kourjot <pkour6188@gmail.com>
2025-04-08 11:11:51 +0530

    token

--- backend/controller/weekWorkout.js
+++ backend/controller/weekWorkout.js
@@ -6,10 +6,10 @@ const jwtKey = process.env.JWT_SECRET_KEY;
 const caloriesBrn = { running: 20, weightlifting: 7, yoga: 5, cardio: 15 };
 const daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
-// const decodeToken = (token) => {
-//   if (!token) throw new Error("Authorization token is required");
-//   return jwt.verify(token, jwtKey);
-// };
+const decodeToken = (token) => {
+  if (!token) throw new Error("Authorization token is required");
+  return jwt.verify(token, jwtKey);
+};
 
 const workoutUpdate = async (req, res) => {
   const token = req.headers["authorization"];
